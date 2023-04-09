fab288ab5590c615853e9de8703a090690457bb6
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-04-09 10:12:05 -0300

    Componente scrollToTop agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+
 import Navbar from './components/Navbar'
-import design from './assets/design.png'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import Inicio from './components/Inicio'
-import Nosotros from './components/Nosotros'
+
 import TemplateContainer from './components/templates/TemplateContainer'
 import PlanesContainer from './components/planes/PlanesContainer'
 import Contacto from './components/Contacto'
 import TemplateDetailContainer from './components/templates/TemplateDetailContainer'
 import Footer from './components/Footer'
 import Faqs from './components/Faqs'
+import ScrollToTop from './components/ScrollToTop'
 
 
 function App() {
@@ -20,6 +20,8 @@ function App() {
 
   return (
     <BrowserRouter>
+
+    <ScrollToTop />
     <div className="App  w-full bg-[#0F172A]  min-h-[100vh] ">
 
      
